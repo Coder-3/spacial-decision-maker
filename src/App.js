968fd36be42da1babdd972a915ef53b6c324e791
Cedric Quenette <cedricquenette@outlook.com>
2021-09-28 17:49:22 +1000

    Removed unecessary files. Adding and removing of inputs and input groups now possible.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+const Thing = ({ numberOfDimensions }) => {
+  return (
+    <div>
+      {[...Array(numberOfDimensions)].map((_, i) =>
+        <input type="number" key={i} />
+      )}
+    </div>
+  )
+}
 
 function App() {
+  const [numberOfThings, setNumberOfThings] = useState(1)
+  const [numberOfDimensions, setNumberOfDimensions] = useState(1)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    console.log(event.target)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        {[...Array(numberOfThings)].map((_, i) => 
+          <Thing numberOfDimensions={numberOfDimensions} key={i} />
+        )}
+        <button type="submit">calculate</button>
+      </form>
+      <button onClick={() => setNumberOfDimensions(numberOfDimensions + 1)}>add dimension</button>
+      <button onClick={() => setNumberOfThings(numberOfThings + 1)}>new thing</button>
     </div>
-  );
+  )
 }
 
 export default App;
